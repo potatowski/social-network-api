68aab55aec3998abe6f51ce334a8981b94ee9faa
Vitor Rocha <deus.1928@gmail.com>
2023-01-27 17:27:17 -0400

    feat: sql to create database and table user

---
+++ app/sql/sql.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS socialnetwork;
+
+USE socialnetwork;
+
+CREATE TABLE users(
+    id int auto_increment primary key,
+    name VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    created TIMESTAMP DEFAULT current_timestamp()
+) ENGINE=INNODB;
