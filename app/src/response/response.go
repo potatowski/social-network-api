d4a47f14cc91765fea0a17a5d2e083d0d0c01f0b
Vitor Rocha <deus.1928@gmail.com>
2023-01-30 00:44:16 -0400

    feat: package to return response

---
+++ app/src/response/response.go
@@ -0,0 +1,25 @@
+package response
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// JSON return a response to request in JSON format
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.WriteHeader(statusCode)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Error return an error to request in JSON format
+func Error(w http.ResponseWriter, statusCode int, err error) {
+	JSON(w, statusCode, struct {
+		Erro string `json:"erro"`
+	}{
+		Erro: err.Error(),
+	})
+}
