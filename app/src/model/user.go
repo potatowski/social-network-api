3a586e275503e97b57efea242ba90ce041c175ed
Vitor Rocha <deus.1928@gmail.com>
2023-01-30 00:13:11 -0400

    improvement(model): added attribute email in struct user

--- app/src/model/user.go
+++ app/src/model/user.go
@@ -6,6 +6,7 @@ type User struct {
 	ID       uint64 `json:"id,omitempty"`
 	Name     string `json:"name,omitempty"`
 	Username string `json:"username,omitempty"`
+	Email    string `json:"email,omitempty"`
 	Password string
 	Created  time.Time `json:"created,omitempty"`
 }
