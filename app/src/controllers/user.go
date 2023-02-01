cb19d22f979f05dcdd8e39fecf3be0939d74089b
Vitor Rocha <deus.1928@gmail.com>
2023-01-31 23:07:48 -0400

    improvement: check user field before save in database

--- app/src/controllers/user.go
+++ app/src/controllers/user.go
@@ -23,6 +23,11 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if err = user.Prepare(); err != nil {
+		response.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
 	db, err := database.Connect()
 	if err != nil {
 		response.Error(w, http.StatusInternalServerError, err)
--- app/src/model/user.go
+++ app/src/model/user.go
@@ -1,6 +1,10 @@
 package model
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type User struct {
 	ID       uint64 `json:"id,omitempty"`
@@ -9,4 +13,40 @@ type User struct {
 	Email    string `json:"email,omitempty"`
 	Password string
 	Created  time.Time `json:"created,omitempty"`
+}
+
+// Prepare call functions to check and format user fields
+func (user *User) Prepare() error {
+	if err := user.check(); err != nil {
+		return err
+	}
+
+	user.format()
+	return nil
+}
+
+func (user *User) check() error {
+	if user.Name == "" {
+		return errors.New("Name is obrigatory")
+	}
+
+	if user.Username == "" {
+		return errors.New("Username is obrigatory")
+	}
+
+	if user.Email == "" {
+		return errors.New("Email is obrigatory")
+	}
+
+	if user.Password == "" {
+		return errors.New("Password is obrigatory")
+	}
+
+	return nil
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Username = strings.TrimSpace(user.Username)
+	user.Email = strings.TrimSpace(user.Email)
 }
