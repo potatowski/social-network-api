a44df93c7c457a3a2928a25a671588647343a546
Vitor Rocha <deus.1928@gmail.com>
2023-02-16 08:53:44 -0400

    improvement(model): change type of id and likes in post

--- app/src/model/post.go
+++ app/src/model/post.go
@@ -5,10 +5,10 @@ import "time"
 // Post represents a publication created by a user
 type Post struct {
 	UUID    string    `json:"uuid,omitempty"`
-	ID      int64     `json:"id,omitempty"`
+	ID      uint64    `json:"id,omitempty"`
 	Title   string    `json:"title,omitempty"`
 	Body    string    `json:"content,omitempty"`
-	Likes   int64     `json:"likes"`
+	Likes   uint64    `json:"likes"`
 	User    *User     `json:"user,omitempty"`
 	Created time.Time `json:"created,omitempty"`
 }
