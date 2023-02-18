3b3fa146c83e949b2dde66c0e1f31787ad602c77
Vitor Rocha <deus.1928@gmail.com>
2023-02-17 22:19:30 -0400

    improvement(db): query to insert data about post

--- app/sql/data.sql
+++ app/sql/data.sql
@@ -10,3 +10,9 @@ values
 (1, 2),
 (3, 1),
 (1, 3);
+
+INSERT INTO post(uuid, title, body, user_id)
+VALUES 
+("dbc08004-763d-4481-844b-2c23e68c63da", "title 1", "body 1", 1),
+("97225a4c-d312-4676-9c16-0456fe5dede2", "title 2", "body 2", 2),
+("ad6d4a0c-1e56-40d9-b234-a140d9e88778", "title 3", "body 3", 3);
