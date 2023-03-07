f15104bbe18358581083cdbc7f2471dd22d8a617
Vitor Rocha <deus.1928@gmail.com>
2023-03-06 22:54:08 -0400

    improvement: change files in folder app/sql to docker/mysql

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -8,15 +8,36 @@ CREATE TABLE user(
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
-    created TIMESTAMP DEFAULT current_timestamp(),
-    removed BOOLEAN DEFAULT 0
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    removed BOOLEAN DEFAULT false
 ) ENGINE=INNODB;
 
 CREATE TABLE follower(
     user_id INT NOT NULL,
-    follower_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+    follower_id INT NOT NULL,
     FOREIGN KEY (follower_id) REFERENCES user(id) ON DELETE CASCADE,
-    created TIMESTAMP DEFAULT current_timestamp(),
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     PRIMARY KEY (user_id, follower_id)
+) ENGINE=INNODB;
+
+CREATE TABLE post(
+    uuid VARCHAR(36) NOT NULL UNIQUE,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    body VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    removed BOOLEAN DEFAULT false,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+    created TIMESTAMP DEFAULT current_timestamp()
+) ENGINE=INNODB;
+CREATE INDEX idx_post_uuid ON post (uuid);
+
+CREATE TABLE post_like(
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+    post_id INT NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES post(id) ON DELETE CASCADE,
+    `type` INT NOT NULL DEFAULT 1,
+    PRIMARY KEY (user_id, post_id)
 ) ENGINE=INNODB;
