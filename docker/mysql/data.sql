d5deef40a0696022f2f08e0270bdc6cb4ed8e276
Vitor Rocha <deus.1928@gmail.com>
2023-03-07 14:46:46 -0400

    improvement: added new values in sql to added data in database development

--- docker/mysql/data.sql
+++ docker/mysql/data.sql
@@ -16,3 +16,21 @@ VALUES
 ("dbc08004-763d-4481-844b-2c23e68c63da", "title 1", "body 1", 1),
 ("97225a4c-d312-4676-9c16-0456fe5dede2", "title 2", "body 2", 2),
 ("ad6d4a0c-1e56-40d9-b234-a140d9e88778", "title 3", "body 3", 3);
+
+INSERT INTO post_like(user_id, post_id, `type`)
+VALUES
+(1, 1, 1),
+(2, 1, 1),
+(3, 1, 1),
+(1, 2, 1),
+(2, 2, 1),
+(3, 2, 1),
+(1, 3, 1),
+(2, 3, 1),
+(3, 3, 1);
+
+INSERT INTO comment(uuid, body, user_id, post_id)
+VALUES
+("a0b0c0d0-e1f1-2a3a-4b5b-6c7c8d9e0f1f", "comment 1", 1, 1),
+("a0b0c0d0-e1f1-2a3a-4b5b-6c7c8d9e0f2f", "comment 2", 2, 2),
+("a0b0c0d0-e1f1-2a3a-4b5b-6c7c8d9e0f3f", "comment 3", 3, 3);
